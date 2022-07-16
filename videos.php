c67471ea4d29cf93d4c3d98f5666b6faeb6ffda1
Ayabhatt <108340961+Ayabhatt@users.noreply.github.com>
2022-07-16 16:29:57 +0530

    Add files via upload

---
+++ videos.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Real Haven</title>
+  </head>
+  <body>
+    
+  <nav class="navbar bg-dark">
+  <div class="container">
+    <a class="navbar-brand mb-5 mr-5" href="#">
+      <img src="mylogo2.jpg" alt="" width="300" height="50">
+    </a>
+  </div>
+</nav>
+      
+<div class="dropdown d-flex justify-content-center mt-2">
+    <button class="btn btn-secondary dropdown-toggle " style=" height:40%; width:10%;" type="button" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+    Category
+    </button>
+    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+      <li><a class="dropdown-item active" href="#">Milf</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">Lesbian</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">lgbt</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">bdsm</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">lesbian</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><a id="cate" class="dropdown-item" href="#">desi</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><button type="button" id="close" data-bs-dismiss="offcanvas" aria-label="Close">close</button>
+        <style>
+          #close{
+            margin-left: 55px;
+            margin-right: auto;
+            opacity: 0.6;
+            color: black;
+            
+          }
+          #cate{
+            color: black;
+          }
+        </style>
+  </div></li>
+  <!-- <form method="post" action="process.php">
+    Name : <input type="text" class="form-example" name="user_name" placeholder="Enter Your Name" /><br />
+    Email : <input type="email" class="form-example" name="user_email" placeholder="Enter Your Email" /><br />
+    Message : <textarea class="form-example" name="user_text"></textarea><br />
+    <input type="submit" value="Submit" />
+    </form> -->
+    
+    </ul>
+  </div>
+  
+ 
+
+<body>
+  
+ 
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+</body>
+<iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <hr>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <hr>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <iframe class="videos mb-4 mt-4" width="560" height="315" src="https://www.youtube.com/embed/y881t8ilMyc" frameborder="0" allowfullscreen></iframe>
+  </iframe>
+  <hr>
+  
+
+</body>
+
+<style>
+  .videos{
+    margin-left: 60px;
+  }
+</style>
+<button class="btn btn-primary" style="margin-left:41%; height:40%; width:20%;" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Click here!</button>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasRightLabel"></h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<style>
+  #offcanvasRight{
+    width:32%;
+  }
+  
+#loading-img{
+display:none;
+}
+.response_msg{
+margin-top:10px;
+margin-bottom: 30rem;
+font-size:13px;
+background:#E5D669;
+color:#ffffff;
+width:250px;
+padding:3px;
+display:none;
+
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<h1><img src="unnamed.png" width="80px">Send us your favourite video</h1>
+<form name="contact-form" action="" method="post" id="contact-form">
+<div class="form-group">
+<label for="Name">Name</label>
+<input type="text" class="form-control" style="width:75%;" name="your_name" placeholder="Name" required>
+</div>
+<div class="form-group">
+<label for="exampleInputEmail1">Email address</label>
+<input type="email" class="form-control" style="width:75%;" name="your_email" placeholder="Email" required>
+</div>
+<div class="form-group">
+<label for="Phone">Phone</label>
+<input type="text" class="form-control" style="width:75%;" name="your_phone" placeholder="Phone" required>
+</div>
+<div class="form-group">
+<label for="comments">video link</label>
+<textarea name="comments" class="form-control" style="width:75%;" rows="3" cols="28" rows="5" placeholder="Comments"></textarea> 
+</div>
+<button type="submit" class="btn btn-primary" style="margin-bottom:50%" name="submit" value="Submit" id="submit_form">Submit</button>
+<img src="giphy.gif" style="width: 30px; length:30px;" id="loading-img">
+</form>
+<div class="response_msg"></div>
+</div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+$("#contact-form").on("submit",function(e){
+e.preventDefault();
+if($("#contact-form [name='your_name']").val() === '')
+{
+$("#contact-form [name='your_name']").css("border","1px solid red");
+}
+else if ($("#contact-form [name='your_email']").val() === '')
+{
+$("#contact-form [name='your_email']").css("border","1px solid red");
+}
+else
+{
+$("#loading-img").css("display","block");
+var sendData = $( this ).serialize();
+$.ajax({
+type: "POST",
+url: "get_response.php",
+data: sendData,
+success: function(data){
+$("#loading-img").css("display","none");
+$(".response_msg").text(data);
+$(".response_msg").slideDown().fadeOut(3000);
+$("#contact-form").find("input[type=text], input[type=email], textarea").val("");
+}
+});
+}
+});
+$("#contact-form input").blur(function(){
+var checkValue = $(this).val();
+if(checkValue != '')
+{
+$(this).css("border","6px solid #eeeeee");
+}
+});
+});
+</script>
+  </div>
+</div>
+  <style>
+  .offcanvas-width{
+    height:50%:
+  }
+  </style>
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"  src="unnamed.png"><use ></use></svg>
+      <img src="mylogo2.jpg" width="150" height="32" alt="">
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+  </footer>
+</div>
+  
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
